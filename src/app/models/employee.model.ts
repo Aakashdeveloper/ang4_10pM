4b94267b0a85895d2a516c0c93ee9190778d4af1
AAKASH HANDA <AAKASH HANDA>
2017-09-11 22:56:26 +0530

    A

---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,9 @@
+export class Employee{
+    constructor(
+        public firstName: string,
+        public lastName: string,
+        public emp_type: boolean,
+        public pay_type:string
+
+    ){}
+}
