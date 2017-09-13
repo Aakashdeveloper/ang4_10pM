cdf627682443512cd5b8e873105dd4ddb6c3cd5d
AAKASH HANDA <AAKASH HANDA>
2017-09-13 22:38:16 +0530

    A

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,15 @@ import {Employee} from '../models/employee.model'
 export class HomeComponent {
   lang =["Java","Python","Javascript"];
   model = new Employee('Aakash','handa',true,'cash',"python",9643018019);
+  hasPrimaryLanguageError = false;
+  validatePrimaryLanguage(event){
+    if(this.model.primaryLanguage ==='default')
+      this.hasPrimaryLanguageError=true;
+    else
+      this.hasPrimaryLanguageError=false;
 
+    console.log("lang:::"+this.model.primaryLanguage);
+  }
   firstNameToUpperCase(value: string){
     if(value.length>0)
       this.model.firstName= value.charAt(0).toUpperCase() +value.slice(1);
